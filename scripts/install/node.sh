1494a35900bdaa3c27cca9074fabcc28bee2d06a
Austin Wei <awei01@gmail.com>
2015-02-06 21:18:24 -0500

    fixing default -y for node.sh

--- scripts/install/node.sh
+++ scripts/install/node.sh
@@ -12,7 +12,7 @@ hash node 2>/dev/null 2>&1 && {
 	echo 'Node already installled'
 } || {
 	echo 'Installing nodejs...'
-	apt-get install nodejs
+	apt-get install nodejs -y
 }
 
 # install npm if it hasn't been
@@ -20,6 +20,6 @@ hash npm 2>/dev/null 2>&1 && {
 	echo 'NPM already installed'
 } || {
 	echo 'Installing NPM...'
-	apt-get install npm
+	apt-get install npm -y
 }
 
