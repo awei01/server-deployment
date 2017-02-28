8d29895edefd16a616695847e3d728a20550779b
Austin Wei <awei01@gmail.com>
2017-02-28 16:53:17 -0500

    updating install of php5.6

--- scripts/install/php-apache.sh
+++ scripts/install/php-apache.sh
@@ -12,12 +12,12 @@ hash php 2>/dev/null 2>&1 && {
 	echo 'PHP & Apache already installled'
 } || {
 	echo 'Installing...'
-	add-apt-repository ppa:ondrej/php5-5.6 -y
+	add-apt-repository ppa:ondrej/php -y
 	apt-get update
-	apt-get install php5 -y
-	apt-get install php5-curl -y
-	apt-get install php5-mcrypt -y
-	apt-get install php5-mysql -y
+	apt-get install php5.6 -y
+	apt-get install php5.6-curl -y
+	apt-get install php5.6-mcrypt -y
+	apt-get install php5.6-mysql -y
 	a2enmod rewrite
 	RESTART=true
 }
